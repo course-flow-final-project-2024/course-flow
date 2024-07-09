a8804052f188a97da9f1e602df43c33540f33d81
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-07-09 11:49:00 +0700

    fix: fix navbar and textbox padding

--- src/pages/homepage/heroSection.jsx
+++ src/pages/homepage/heroSection.jsx
@@ -4,7 +4,7 @@ import BackgroundVectors from "./backgroundVectors";
 function HeroSection() {
   return (
     <div>
-      <div className="w-[90%] h-max relative left-[50%] translate-x-[-50%] top-10 z-10 flex flex-col items-start gap-5  ">
+      <div className="w-full px-4 h-max relative left-[50%] translate-x-[-50%] top-10 z-10 flex flex-col items-start gap-5  ">
         <div className="w-full text-4xl leading-10 text-black ">
           <span className="block font-medium">
             Best Virtual Classroom Software

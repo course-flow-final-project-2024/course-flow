0d83a608c510061c5cd53823f89dcf52f1724778
nattawara <nattawara.jtk@gmail.com>
2024-07-10 10:30:09 +0700

    feat: add pagination

--- src/components/admin/courses-table.js
+++ src/components/admin/courses-table.js
@@ -9,16 +9,25 @@ import {
   Flex,
   Image,
   Box,
+  Button,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const AdminCoursesList = () => {
   const [courseList, setCourseList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage] = useState(8);
+
+  useEffect(() => {
+    getCourseList();
+  }, [currentPage]);
 
   async function getCourseList() {
     try {
-      const result = await axios.get("/api/course");
+      const result = await axios.get(
+        "/api/course?page=${currentPage}&perPage=${perPage}"
+      );
 
       setCourseList(result.data.courses);
     } catch (error) {
@@ -26,9 +35,19 @@ const AdminCoursesList = () => {
     }
   }
 
-  useEffect(() => {
-    getCourseList();
-  }, []);
+  const totalPages = Math.ceil(courseList.length / perPage);
+
+  const startIndex = (currentPage - 1) * perPage;
+  const endIndex = Math.min(startIndex + perPage, courseList.length);
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
   return (
     <Box>
       <div>
@@ -36,19 +55,19 @@ const AdminCoursesList = () => {
           <Table variant="simple">
             <Thead>
               <Tr bg="gray.200" color="gray.800" borderRadius="md">
-                <Th></Th>
-                <Th>Image</Th>
-                <Th>Course Name</Th>
-                <Th>Lesson</Th>
-                <Th>Price</Th>
-                <Th>Created date</Th>
-                <Th>Updated date</Th>
-                <Th>Action</Th>
+                <Th width="48px">no.</Th>
+                <Th width="96px">Image</Th>
+                <Th width="268px">Course Name</Th>
+                <Th width="105px">Lesson</Th>
+                <Th width="105px">Price</Th>
+                <Th width="188px">Created date</Th>
+                <Th width="188px">Updated date</Th>
+                <Th width="120px">Action</Th>
               </Tr>
             </Thead>
             <Tbody>
-              {courseList.map((item, index) => {
-                const recordNumber = index + 1;
+              {courseList.slice(startIndex, endIndex).map((item, index) => {
+                const recordNumber = startIndex + index + 1;
                 return (
                   <Tr
                     bg="white"
@@ -56,17 +75,18 @@ const AdminCoursesList = () => {
                     height="88px"
                     borderbuttom="1px"
                     borderColor="gray.200"
-                    key={item.course_id}
+                    key={index}
                   >
                     <Td>{recordNumber}</Td>
                     <Td>
                       <Image
                         src={item.course_image}
+                        // src="/logo/CourseFlow-logo.svg"
                         alt="course-image"
                         boxSize={47}
                       />
                     </Td>
-                    <Td>{item.course_name}</Td>
+                    <Td wordBreak="break-word">{item.course_name}</Td>
                     <Td>x Lessons</Td>
                     <Td>{item.price}</Td>
                     <Td>{item.created_at}</Td>
@@ -83,6 +103,23 @@ const AdminCoursesList = () => {
             </Tbody>
           </Table>
         </TableContainer>
+        <Flex justify="center" mt={4}>
+          <Button
+            variant="outline"
+            disabled={currentPage === 1}
+            onClick={handlePrevPage}
+            mr={2}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            disabled={currentPage === totalPages}
+            onClick={handleNextPage}
+          >
+            Next
+          </Button>
+        </Flex>
       </div>
     </Box>
   );

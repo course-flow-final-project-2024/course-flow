6ac1b37e1981d73ea03fbe8e857c5099d1ab4ddd
tle <tlemakuz@gmail.com>
2024-07-10 08:43:16 +0700

    Fix:authfunc

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,22 +1,40 @@
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
-import { SupabaseAdapter } from '@next-auth/supabase-adapter';
-import { createClient } from '@supabase/supabase-js';
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import { SupabaseAdapter } from "@next-auth/supabase-adapter";
+import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
 
 export default NextAuth({
   providers: [
-    Providers.Email({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
+    //เงื่อนไขตัวlogin ด้วย email และ passwordน่ะคับ
+    Providers.Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        const { email, password } = credentials;
+        const { data, error } = await supabase.auth.signInWithPassword({
+          email,
+          password,
+        });
+
+        if (error || !data.user) {
+          //ถ้าเกิดว่า ไม่ได้loginด้วยข้อมุลที่regisไว้หรือข้อมุลuserในdata  ส่งerror
+          throw new Error("Invalid credentials");
+        }
+
+        return { id: data.user.id, email: data.user.email };
+        //ในกรณีที่ไม่เกิด error และมีข้อมูลผู้ใช้ จะทำการส่งข้อมูลกลับไปid และ email ของuserจาก data.user ออกไปเพื่อใช้งานต่อในส่วนอื่นของโปรแกรม เกบในรูปแบบ objectน่ะคับ"{ }"
+      },
     }),
-    /*"
-    เว้นไว้เผื่อไว้ใส่
-    providerตัวอื่น"
-    */
   ],
-  adapter: SupabaseAdapter(supabase),
+  adapter: SupabaseAdapter(supabase), //ดูวิธีใช้supabaseได้ในdocของnextAuth.js ในหมวด adaptersคับแล้วเลือกsupabase มันจะมีรูปแบบcoppyมาได้เลย แล้มาปรับเอา
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     jwt: true,
@@ -38,3 +56,9 @@ export default NextAuth({
     },
   },
 });
+/*ไลบารี่ที่ต้องติดตั้ง
+ npm install next-auth
+npm install @supabase/supabase-js
+npm install @next-auth/supabase-adapter
+npm install @next-auth/jwt
+*/

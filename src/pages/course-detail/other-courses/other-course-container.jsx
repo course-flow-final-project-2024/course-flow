3809b586a198416666a1d5c43d8d83ace613aad6
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-07-10 21:06:55 +0700

    feat: create coursedetail page layout with main detail section

---
+++ src/pages/course-detail/other-courses/other-course-container.jsx
@@ -0,0 +1,9 @@
+function OtherCourses() {
+  return (
+    <div className="w-full h-max px-10 py-4">
+      <div className="w-full h-max flex flex-col gap-8 border-2"></div>
+    </div>
+  );
+}
+
+export default OtherCourses;

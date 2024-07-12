28e477285578947ac21c6e3b4fe103b54d3cc7fe
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-07-12 23:24:39 +0700

    feat: adjust landing page padding for desktop screen

--- src/components/homepage/second-section/second-section-container.jsx
+++ src/components/homepage/second-section/second-section-container.jsx
@@ -6,7 +6,7 @@ function HomepageSecondSection() {
     "Duis aute irure dolor in reprehenderit in voluptate velit es se cillum dolore eu fugiat nulla pariatur. Excepteur sint.";
   return (
     <div className="relative w-full h-max flex sm:flex-col ">
-      <div className="flex flex-col px-4 py-16 gap-8 sm:px-20 ease-in-out duration-200 lg:px-40 lg:gap-24 min-[1800px]:px-96 ">
+      <div className="flex flex-col px-4 py-16 gap-8 sm:px-20 ease-in-out duration-200 lg:px-40 lg:gap-24 min-[1800px]:px-96">
         <SectionTwoBox
           boxTitle="Learning experience has been enhanced with new technologies"
           featureBanner="homepage/second-section-materials/section-2-banner.svg"

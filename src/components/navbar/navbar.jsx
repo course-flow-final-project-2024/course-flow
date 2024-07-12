2272d284e3dd5a9968d31a883bd49698537e683b
RcnonS <ratchanon.somprasong@gmail.com>
2024-07-12 18:02:42 +0700

    feat: create lesson table on add-course page

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,7 +4,7 @@ import LoginButton from "./login-button.jsx";
 
 function Navbar() {
   return (
-    <div className="w-full h-14 bg-white flex justify-between shadow-md px-4 ease-in-out duration-200 sm:px-16 lg:px-40 lg:h-[88px]">
+    <div className="w-full h-14 bg-white flex justify-between shadow-md px-4 ease-in-out duration-200 sm:px-16 xl:px-40 lg:h-[88px]">
       <Image
         src="/logo/CourseFlowLogo.svg"
         width={117}
@@ -14,7 +14,7 @@ function Navbar() {
       />
       <div className=" h-full w-3/6 flex items-center justify-end gap-4 lg:gap-8">
         <Link
-          href="/"
+          href="/courses"
           className="text-[#191C77] font-bold text-sm lg:text-base"
         >
           Our Courses

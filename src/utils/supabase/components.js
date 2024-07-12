95c7432dd5dd02f189d36f887af8cf01f55e6028
tle <tlemakuz@gmail.com>
2024-07-12 15:29:22 +0700

    fix: componentname

--- src/utils/supabase/components.js
+++ src/utils/supabase/components.js
@@ -7,7 +7,7 @@ export function createClient() {
   if (!supabaseInstance) {
     supabaseInstance = createBrowserClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+      process.env.NEXT_PUBLIC_SUPABASE_KEY
     );
   }
   

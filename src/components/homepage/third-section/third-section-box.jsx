d690ecfe52ab244310c3c42bc2a7de5a438e30f4
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-07-12 23:24:39 +0700

    feat: adjust landing page padding for desktop screen

--- src/components/homepage/third-section/third-section-box.jsx
+++ src/components/homepage/third-section/third-section-box.jsx
@@ -2,21 +2,21 @@ import Image from "next/image";
 
 function SectionThreeBox(props) {
   return (
-    <div className="w-full h-max flex flex-col gap-1 lg:w-max">
-      <div className="w-full h-auto object-fit flex flex-col items-center">
+    <div className="w-full h-full flex flex-col gap-2 lg:w-full ">
+      <div className="w-full h-max object-cover flex flex-col items-center ">
         <Image
           src={props.imageUrl}
-          width={343}
+          width={399}
           height={403.53}
-          className="w-full max-w-[450px]"
+          className="w-full"
           alt="Instructor Image"
         />
       </div>
-      <div className="flex flex-col">
-        <p className="text-xl text-black block text-center sm:text-2xl ease-in-out duration-200">
+      <div className="flex flex-col lg">
+        <p className="text-xl text-black block text-center sm:text-xl xl:text-2xl ease-in-out duration-200">
           {props.instructorName}
         </p>
-        <p className="text-blueRole block text-center sm:text-lg ease-in-out duration-200">
+        <p className="text-blueRole block text-center sm:text-base xl:text-lg  ease-in-out duration-200">
           {props.role}
         </p>
       </div>

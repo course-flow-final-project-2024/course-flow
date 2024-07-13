fbd7e8321556162eb6481bbdb4151fb082bd1922
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-07-13 16:33:35 +0700

    style: make side course card detail stick when scroll

--- src/components/course-detail-section/main/side-course-card.jsx
+++ src/components/course-detail-section/main/side-course-card.jsx
@@ -1,6 +1,8 @@
+import Link from "next/link";
+
 function SideCourseCard() {
   return (
-    <div className="w-full h-max shadow-lg py-8 px-6 flex flex-col gap-6 rounded-lg lg:h-[20%] ">
+    <div className="w-full h-max shadow-lg py-8 px-6 flex flex-col gap-6 rounded-lg sticky top-10 ">
       <div className="w-full text-sm text-[#F47E20]">Course</div>
       <div className="w-full flex flex-col gap-2">
         <h3 className="font-medium md:text-xl xl:text-2xl text-black">
@@ -10,16 +12,22 @@ function SideCourseCard() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </span>
       </div>
-      <h3 className="w-full font-medium md:text-xl xl:text-2xl text-[#646D89]">
+      <h3 className="w-full h-max font-medium md:text-xl xl:text-2xl text-[#646D89]">
         THB 3,559.00
       </h3>
       <div className="border-t-[1px] flex flex-col gap-4 pt-8">
-        <button className="h-max py-4 rounded-xl bg-white text-orange-500 border-[1px] border-orange-500 active:bg-gray-100 hover:text-[#FBAA1C] hover:border-[#FBAA1C] focus:ring-2 ring-violet-300 text-sm font-bold ease-in-out duration-200 ">
+        <Link
+          href="/login"
+          className="h-max py-4 rounded-xl bg-white text-[#F47E20]  border-[1px] border-orange-500 active:bg-gray-100 hover:text-[#FBAA1C] hover:border-[#FBAA1C] focus:ring-2 ring-violet-300   text-sm font-bold ease-in-out duration-200 text-center"
+        >
           Get in Desire Course
-        </button>
-        <button className="h-max py-4 rounded-xl bg-[#2F5FAC] text-white  active:bg-[#183056] hover:bg-[#5483D0] focus:ring-2 ring-violet-300 text-sm font-bold ease-in-out duration-200">
+        </Link>
+        <Link
+          href="/login"
+          className="h-[max] py-4 rounded-xl bg-[#2F5FAC] text-white active:bg-[#183056] hover:bg-[#5483D0] focus:ring-2 ring-violet-300 text-sm font-bold ease-in-out duration-200 text-center"
+        >
           Subscribe This Course
-        </button>
+        </Link>
       </div>
     </div>
   );

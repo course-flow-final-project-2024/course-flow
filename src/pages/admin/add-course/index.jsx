c6192400a3fbc50580f9cb7fd5cd539146235b7d
RcnonS <ratchanon.somprasong@gmail.com>
2024-07-15 18:43:12 +0700

    feat: create context api fot lesson and sub-lesson data

--- src/pages/admin/add-course/index.jsx
+++ src/pages/admin/add-course/index.jsx
@@ -1,21 +1,27 @@
 import AdminSidebar from "@/components/admin/sidebar";
 import AdminCreatingHeader from "@/components/admin/header/creating-page";
 import { AdminLessonSection } from "@/components/admin/add-course/section-lesson";
+import React, { createContext, useState } from "react";
+
+export const LessonDataContext = createContext();
 
 export default function AddNewCourse() {
+  const [lesson, setLesson] = useState([]);
+
   return (
-    <>
-      <div className="flex flex-grow w-full mx-auto h-full">
-        <div>
-          <AdminSidebar />
+    <React.Fragment>
+      <LessonDataContext.Provider value={{ lesson, setLesson }}>
+        <div className="flex flex-grow w-full mx-auto h-full">
+          <div>
+            <AdminSidebar />
+          </div>
+          <div className="flex flex-col w-full bg-gray-100">
+            <AdminCreatingHeader section="Add Course" />
+            <div className="h-[1520px] border border-black">ploy</div>
+            <AdminLessonSection lesson={lesson} setLesson={setLesson} />
+          </div>
         </div>
-        <div className="flex flex-col w-full bg-gray-100">
-          <AdminCreatingHeader section="Add Course" />
-          <div className="h-[1520px] border border-black">ploy</div>
-          {/* Nonnnnnn */}
-          <AdminLessonSection />
-        </div>
-      </div>
-    </>
+      </LessonDataContext.Provider>
+    </React.Fragment>
   );
 }

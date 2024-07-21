ea520b70bf9ed05f559c5fdf9824deaa6a274d07
tle <tlemakuz@gmail.com>
2024-07-21 09:06:36 +0700

    feat: profileform

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -24,7 +24,14 @@ export default async function handler(req, res) {
       return res.status(401).json({ error: error.message || "Login failed" });
     }
 
-    return res.status(200).json({ message: "Login successful", user });
+    const session = await supabase.from("loginsession").insert({userid: user.user_id}).select()
+    console.log("session", session)
+    if (session.error) {
+      throw new Error(session.error)
+    }
+
+    return res.status(200).json({ message: "Login successful", token: session.data.sessionId });
+    // return res.status(200).json({ message: "Login successful", user});
   } catch (error) {
     console.error("Error signing in:", error.message);
     return res.status(500).json({ error: "Failed to sign in" });

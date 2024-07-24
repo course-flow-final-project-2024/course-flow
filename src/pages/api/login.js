97ae9a47628d05231097174fceabce30fcd2d056
tle <tlemakuz@gmail.com>
2024-07-24 09:48:36 +0700

    fix: userprofile and login

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,4 +1,4 @@
-import { supabase } from "../../../lib/supabase";
+import { supabase } from "../../../../lib/supabase";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
@@ -24,14 +24,15 @@ export default async function handler(req, res) {
       return res.status(401).json({ error: error.message || "Login failed" });
     }
 
-    const session = await supabase.from("loginsession").insert({userid: user.user_id}).select()
-    console.log("session", session)
+    
+    const session = await supabase.from("loginsession").insert({user_email: user.email}).select()
+  
+    
     if (session.error) {
       throw new Error(session.error)
     }
 
-    return res.status(200).json({ message: "Login successful", token: session.data.sessionId });
-    // return res.status(200).json({ message: "Login successful", user});
+    return res.status(200).json({ message: "Login successful",  token: session.data[0].sessionId });
   } catch (error) {
     console.error("Error signing in:", error.message);
     return res.status(500).json({ error: "Failed to sign in" });

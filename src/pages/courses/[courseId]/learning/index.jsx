44ada821e872d77dba7e3dafb88399bddae84703
Athikhom_ra <athikhom_ra@outlook.com>
2024-07-24 16:17:31 +0700

    feat: create api course_learning

--- src/pages/courses/[courseId]/learning/index.jsx
+++ src/pages/courses/[courseId]/learning/index.jsx
@@ -3,18 +3,25 @@ import ActionBar from "@/components/course-learning-section/action-bar";
 import CommonFooter from "@/components/footer/common-footer";
 import CoursesContent from "@/components/course-learning-section/content";
 import CoursesProgress from "@/components/course-learning-section/progress";
+import getUserCourseById from "./get-user-course-by-id";
+import { createContext, useState } from "react";
+
+export const CoursesDataContext = createContext();
 
 function CourseLearning() {
+  getUserCourseById(17, 5);
   return (
-    <div className="w-full h-max">
-      <Navbar />
-      <div className="w-full h-full flex flex-col sm:flex-row sm:justify-center max-[640px]:items-center ">
-        <CoursesProgress />
-        <CoursesContent />
+    <CoursesDataContext.Provider value={{ lesson, setLesson }}>
+      <div className="w-full h-max">
+        <Navbar />
+        <div className="w-full h-full flex flex-col sm:flex-row sm:justify-center max-[640px]:items-center ">
+          <CoursesProgress />
+          <CoursesContent />
+        </div>
+        <ActionBar />
+        <CommonFooter />
       </div>
-      <ActionBar />
-      <CommonFooter />
-    </div>
+    </CoursesDataContext.Provider>
   );
 }
 

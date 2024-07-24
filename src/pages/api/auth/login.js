c029c930761423d98b36c878af54a7c45f366d56
Athikhom_ra <athikhom_ra@outlook.com>
2024-07-24 16:17:31 +0700

    feat: create api course_learning

--- src/pages/api/auth/login.js
+++ src/pages/api/auth/login.js
@@ -24,15 +24,18 @@ export default async function handler(req, res) {
       return res.status(401).json({ error: error.message || "Login failed" });
     }
 
-    
-    const session = await supabase.from("loginsession").insert({user_email: user.email}).select()
-  
-    
+    const session = await supabase
+      .from("loginsession")
+      .insert({ user_email: user.email })
+      .select();
+
     if (session.error) {
-      throw new Error(session.error)
+      throw new Error(session.error);
     }
 
-    return res.status(200).json({ message: "Login successful",  token: session.data[0].sessionId });
+    return res
+      .status(200)
+      .json({ message: "Login successful", token: session.data[0].sessionId });
   } catch (error) {
     console.error("Error signing in:", error.message);
     return res.status(500).json({ error: "Failed to sign in" });

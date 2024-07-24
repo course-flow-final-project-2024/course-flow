0e525e1f4111f8a47ad3118563d5c5125ae16c43
RcnonS <ratchanon.somprasong@gmail.com>
2024-07-25 00:10:49 +0700

    feat: render api data to each component on learning page

--- src/components/course-learning-section/action-bar.jsx
+++ src/components/course-learning-section/action-bar.jsx
@@ -1,12 +1,17 @@
 import Button from "@/utils/button";
-function ActionBar(prop) {
+function ActionBar({ onNextLesson, onPreviousLesson }) {
   return (
     <div className="w-full h-[100px] flex flex-row justify-between items-center p-4 shadow-sm">
-      <div className="px-1">
-        <a className="text-xl font-bold text-[#2F5FAC]">Previous Lesson</a>
+      <div className="px-1 cursor-pointer">
+        <a
+          className="text-xl font-bold text-[#2F5FAC]"
+          onClick={onPreviousLesson}
+        >
+          Previous Lesson
+        </a>
       </div>
       <div className="w-[160px] flex">
-        <Button text="Next Lesson" style="primary" />
+        <Button text="Next Lesson" style="primary" onClick={onNextLesson} />
       </div>
     </div>
   );
--- src/components/course-learning-section/progress.jsx
+++ src/components/course-learning-section/progress.jsx
@@ -1,10 +1,11 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import LessonAccordion from "./lesson-accordion";
 import { CoursesDataContext } from "@/pages/courses/[courseId]/learning";
 import axios from "axios";
 
 function CoursesProgress() {
-  const { setCourseData } = useContext(CoursesDataContext);
+  const { courseData, setCourseData } = useContext(CoursesDataContext);
+  const [progress, setProgress] = useState(0);
 
   const getCourseData = async () => {
     try {
@@ -25,24 +26,54 @@ function CoursesProgress() {
     }
   };
 
+  const calculateProgress = (lessons) => {
+    let totalSubLessons = 0;
+    let completedSubLessons = 0;
+
+    lessons.forEach((lesson) => {
+      lesson.sub_lessons.forEach((subLesson) => {
+        totalSubLessons++;
+        if (
+          subLesson.user_lessons.some((item) => item.sub_lesson_status_id === 1)
+        ) {
+          completedSubLessons++;
+        }
+      });
+    });
+
+    return totalSubLessons > 0
+      ? (completedSubLessons / totalSubLessons) * 100
+      : 0;
+  };
+
+  useEffect(() => {
+    if (courseData.length > 0) {
+      const course = courseData[0].courses;
+      const progressValue = calculateProgress(course.lessons);
+      setProgress(progressValue);
+    }
+  }, [courseData]);
+
   useEffect(() => {
     getCourseData();
   }, []);
+
+  const course = courseData.length > 0 ? courseData[0].courses : "Loading...";
 
   return (
     <main className="w-full max-w-[350px] h-full mt-[4%] flex flex-col p-4 sm:px-6 sm:py-8 gap-4 rounded-[8px] bg-white shadow-xl">
       <div className="text-sm font-normal text-[#F47E20]">Course</div>
       <div className="header">
-        <h2 className="text-2xl font-medium">Service Design Essentials</h2>
-        <p className="text-base font-normal text-[#646D89]">
-          Lorem ipsum dolor sit amet, conse ctetur adipiscing elit.
-        </p>
+        <h2 className="text-2xl font-medium">{course.course_name}</h2>
+        <p className="text-base font-normal text-[#646D89]">{course.summary}</p>
       </div>
       <div className="percent">
-        <a className="text-sm font-normal text-[#646D89]">15% Complete</a>
+        <a className="text-sm font-normal text-[#646D89]">
+          {progress.toFixed(2)}% Complete
+        </a>
         <progress
           className="progress progress-bar  w-full"
-          value={15}
+          value={progress}
           max="100"
         ></progress>
       </div>

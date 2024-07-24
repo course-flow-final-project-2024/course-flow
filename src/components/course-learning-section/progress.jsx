38c2824941267ff62c109a1c5c061e4da04b101b
RcnonS <ratchanon.somprasong@gmail.com>
2024-07-24 19:19:06 +0700

    refactor: revise context api for course-learning page

--- src/components/course-learning-section/progress.jsx
+++ src/components/course-learning-section/progress.jsx
@@ -1,5 +1,34 @@
+import { useContext, useEffect } from "react";
 import LessonAccordion from "./lesson-accordion";
+import { CoursesDataContext } from "@/pages/courses/[courseId]/learning";
+import axios from "axios";
+
 function CoursesProgress() {
+  const { setCourseData } = useContext(CoursesDataContext);
+
+  const getCourseData = async () => {
+    try {
+      const result = await axios.get(
+        `/api/courses_learning/get-sub-lesson-status`,
+        {
+          params: {
+            userId: 17,
+            courseId: 5,
+          },
+        }
+      );
+      setCourseData(result.data.courses);
+    } catch (error) {
+      return {
+        message: "Server could not read courses due to database connection",
+      };
+    }
+  };
+
+  useEffect(() => {
+    getCourseData();
+  }, []);
+
   return (
     <main className="w-full max-w-[350px] h-full mt-[4%] flex flex-col p-4 sm:px-6 sm:py-8 gap-4 rounded-[8px] bg-white shadow-xl">
       <div className="text-sm font-normal text-[#F47E20]">Course</div>
